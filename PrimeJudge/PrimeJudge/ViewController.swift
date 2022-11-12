5ac73a0b1ab9c18c77fb8a0aae511e1012b54f42
Takuchan <awatykhall850@outlook.com>
2022-11-13 00:21:28 +0900

    素数アプリの完成

--- PrimeJudge/PrimeJudge/ViewController.swift
+++ PrimeJudge/PrimeJudge/ViewController.swift
@@ -8,12 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var primeText: UILabel!
+    
+    var count : Int = 0;
+    @IBOutlet weak var countText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countText.delegate = self
     }
+    
+    
 
-
+    @IBAction func plusbutton(_ sender: Any) {
+        count = count + 1;
+        countText.text = String(count)
+        if(judgePrime(num: count)){
+            primeText.text = "素数"
+            if(count < 2){
+                primeText.text = "判別不可"
+            }
+        }else{
+            primeText.text = "偶数"
+            if(count < 2){
+                primeText.text = "判別不可"
+            }
+        }
+    }
+    
+    @IBAction func minusbutton(_ sender: Any) {
+        count = count - 1;
+        countText.text = String(count);
+        if(judgePrime(num: count)){
+            primeText.text = "素数"
+            if(count < 2){
+                primeText.text = "判別不可"
+            }
+        }else{
+            primeText.text = "偶数"
+            if(count < 2){
+                primeText.text = "判別不可"
+            }
+        }
+    }
+    
+    func judgePrime(num: Int) -> Bool{
+        var booljudge : Bool = true;
+        if(num < 2){
+            
+        }else{
+            for i in 2..<num{
+                if(num % i == 0){
+                    booljudge = false
+                    break;
+                }
+            }
+        }
+        
+        return booljudge;
+        
+    }
+    
 }
 
+extension ViewController:UITextFieldDelegate{
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        countText.keyboardType = UIKeyboardType.numberPad;
+        if countText.text == nil || countText.text!.isEmpty{
+            count = 0;
+            countText.text = String(0)
+        }else{
+            count = Int(countText.text!)!
+            
+        }
+        
+       }
+}
